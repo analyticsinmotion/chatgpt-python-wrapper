1cc5e1aabe66df6fd468f5dbe7fb0dd32ab8d9a2
Ross Armstrong <52817125+rossarmstrong@users.noreply.github.com>
2023-01-13 23:42:25 +1100

    Added user menu to choose between ChatGPT text or image capabilities

--- main.py
+++ main.py
@@ -4,7 +4,7 @@
 # Tested on: Python 3.11
 # PEP8 Exceptions: NONE
 # Description:  This is the main file that controls the execution of the python wrappers for accessing ChatGPT. It
-#               imports the chatgpt_text module and allows users to ask a question.
+#               imports the chatgpt_text, chat_gpt_images modules and allows users to what they want to do.
 #
 #######################################################################################################################
 
@@ -13,11 +13,44 @@ import json
 
 # Import files that contain the python wrappers for text and images
 from chatgpt_text import chatgpt_text
+from chatgpt_images import chatgpt_images
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    question = input("Enter your question: ")
-    result = chatgpt_text(question, output_to_file=True)
+def menu():
+    menu_options = {
+        1: 'ChatGPT Text functionality',
+        2: 'ChatGPT Image Generation functionality',
+        3: 'Exit'
+    }
+
+    for key in menu_options.keys():
+        print(key, '--', menu_options[key])
+
+    menu_option_input = int(input('Please select an option 1 to 3 from the menu above: '))
+
+    # Check what choice was entered and act accordingly
+    if menu_option_input == 1:
+        option1()
+    elif menu_option_input == 2:
+        option2()
+    elif menu_option_input == 3:
+        print('Thanks you. Goodbye')
+        exit()
+    else:
+        print('Invalid option. Please enter a number between 1 and 3.')
+
+
+def option1():
+    input_prompt = input("Enter your text/question/scenario: ")
+    result = chatgpt_text(input_prompt, output_to_file=True)
     result_dict = json.loads(str(result))
-    print("Answer: " + result_dict['choices'][0]['text'].strip('\n'))
+    return print("Answer: " + result_dict['choices'][0]['text'].strip('\n'))
+
+
+def option2():
+    input_prompt = input("Enter a description of the image you would like created: ")
+    chatgpt_images(input_prompt, n=1, output_to_file=True)
+
+
+if __name__ == '__main__':
+    menu()
