8f2c2809200e434be5dd6dbbef66ba5fb6d756c6
Ross Armstrong <52817125+rossarmstrong@users.noreply.github.com>
2023-02-03 13:29:57 +1100

    Modified Module Docstring

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
-#######################################################################################################################
-# Filename: main.py
-# Creation Date: 4/01/2023
-# Tested on: Python 3.11
-# PEP8 Exceptions: NONE
-# Description:  This is the main file that controls the execution of the python wrappers for accessing ChatGPT. It
-#               imports the chatgpt_text, chat_gpt_images modules and allows users to what they want to do.
-#
-#######################################################################################################################
+"""
+This is the main file that controls the execution of the python wrappers for accessing ChatGPT.
+It imports the chatgpt_text, chat_gpt_images modules and allows users to what they want to do.
+
+Functions:
+    chatgpt_text(prompt, model, max_tokens, temperature top_p, frequency_penalty, presence_penalty, n, output_to_file)
+    -> object
+    chatgpt_images(prompt, n, size, output_to_file) -> object
+    chatgpt_moderation(prompt, model, output_to_file) -> tuple
+"""
 
 # Import json
 import json
@@ -18,6 +19,11 @@ from chatgpt_moderation import chatgpt_moderation
 
 
 def menu():
+    """
+    This function displays the menu options in the UI
+
+    :return: Dictionary
+    """
     menu_options = {
         1: 'ChatGPT Text functionality',
         2: 'ChatGPT Image Generation functionality',
@@ -42,6 +48,11 @@ def menu():
 
 
 def option1():
+    """
+    This function takes user input, does a moderation check and then send to ChatGPT for text completion
+
+    :return: str
+    """
     input_prompt = input("Enter your text/question/scenario: ")
     moderation_check = chatgpt_moderation(input_prompt, output_to_file=True)
     if not moderation_check[0]:
@@ -55,6 +66,10 @@ def option1():
 
 
 def option2():
+    """
+    This function takes user input, does a moderation check and then send to ChatGPT for image generation
+    :return: A JSON object with the image details
+    """
     input_prompt = input("Enter a description of the image you would like created: ")
     moderation_check = chatgpt_moderation(input_prompt, output_to_file=True)
     if not moderation_check[0]:
